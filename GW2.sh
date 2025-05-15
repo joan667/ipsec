9664f04a4cb9533de2196e90045d832766e04252
joan667 <44604896+joan667@users.noreply.github.com>
2025-05-15 15:00:06 +0200

    Update GW2.sh

--- GW2.sh
+++ GW2.sh
@@ -1,23 +1,18 @@
 #!/usr/sbin/setkey -f
 
-flush;
-spdflush;
+flush;      
+spdflush;   
 
-add 10.0.3.2 10.0.0.1 esp 0x1565785 -m tunnel \
-    -E aes-cbc 0x0123456789abcdef0123456789abcdef \
-    -A hmac-sha1 0xabcdef0123456789abcdef0123456789abcdef01;
-
-add 10.0.0.1 10.0.3.2 esp 0x1565786 -m tunnel \
+add 10.0.3.2 10.0.0.1 esp 0x1565786 -m tunnel \
     -E aes-cbc 0xfedcba9876543210fedcba9876543210 \
     -A hmac-sha1 0x10fedcba9876543210fedcba9876543210fedcba;
 
-spdadd 2001:db8:1234::3/64 2001:db8:1234::2/64 any -P out ipsec \
-    esp/tunnel/10.0.0.1-10.0.3.2/require;
+add 10.0.0.1 10.0.3.2 esp 0x1565785 -m tunnel \
+    -E aes-cbc 0x0123456789abcdef0123456789abcdef \
+    -A hmac-sha1 0xabcdef0123456789abcdef0123456789abcdef01;
 
-spdadd 2001:db8:1234::2/64 2001:db8:1234::3/64 any -P in ipsec \
+spdadd 2001:db8:1234::3 2001:db8:1234::2 any -P out ipsec \
     esp/tunnel/10.0.3.2-10.0.0.1/require;
 
-
-spdadd 2001:db8:1234::3/64 2001:db8:1234::2/64 any -P in none;
-
-spdadd 2001:db8:1234::2/64 2001:db8:1234::3/64 any -P out none;
+spdadd 2001:db8:1234::2 2001:db8:1234::3 any -P in  ipsec \
+    esp/tunnel/10.0.0.1-10.0.3.2/require;
