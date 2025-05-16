a9de02e34ff645cd327eab80ddf99d762e3d96f2
joan667 <44604896+joan667@users.noreply.github.com>
2025-05-16 07:02:07 +0000

    Update GW2.sh

--- GW2.sh
+++ GW2.sh
@@ -11,8 +11,8 @@ add 10.0.0.1 10.0.3.2 esp 0x1565785 -m tunnel \
     -E aes-cbc 0x0123456789abcdef0123456789abcdef \
     -A hmac-sha1 0xabcdef0123456789abcdef0123456789abcdef01;
 
-spdadd 2001:db8:1234::3 2001:db8:1234::2 any -P out ipsec \
+spdadd 10.0.3.2 10.0.0.1 any -P out ipsec \
     esp/tunnel/10.0.3.2-10.0.0.1/require;
 
-spdadd 2001:db8:1234::2 2001:db8:1234::3 any -P in  ipsec \
+spdadd 10.0.0.1 10.0.3.2 any -P in  ipsec \
     esp/tunnel/10.0.0.1-10.0.3.2/require;
